bd14b39ecde8a4eb506b23268487bc350a3e64ea
santosh <sklearn21@gmail.com>
2020-02-07 11:24:03 +0530

    Added view to polls/views

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,7 @@ def detail(request, question_id):
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
-return HttpResponse(response % question_id)
+    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
